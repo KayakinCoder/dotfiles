3a913daeac5e5573a2f0ea9b27dd9617f698cdbe
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-10 13:40:03 -0700

    Copy .bash_aliases to home directory
    Added copying of .bash_aliases to home directory.

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ done
 # get config here into the home config
 cp -r ./.config/* ~/.config/
 
+# dotfiles
+cp .bash_aliases ~/.bash_aliases
+
 # my aws home
 echo 'export AWS_DEFAULT_REGION=us-west-2' >> ~/.bashrc
