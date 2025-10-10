38cd6203e314318ac9f76e6580660841da1d05f6
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-10 13:27:36 -0700

    Add config file copying to install script
    Copy configuration files to the user's home config directory.

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@ for script in ./tools/preinstall/*.sh; do
   /bin/bash "$script"
 done
 
+# get config here into the home config
+cp -r ./.config/* ~/.config/
+
 # my aws home
 echo 'export AWS_DEFAULT_REGION=us-west-2' >> ~/.bashrc
