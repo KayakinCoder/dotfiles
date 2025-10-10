bdf953a9bb7c0454bb8d89768ad967a5ed51d7da
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-10 14:03:12 +0000

    font install

--- tools/preinstall/starship.sh
+++ tools/preinstall/starship.sh
@@ -1,5 +1,5 @@
 # starship
-.build/tools/install-font.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
+../../helpers/install-font.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 cp /workspaces/devcontainer-fd/.build/tools/starship.toml ~/.config/starship.toml
