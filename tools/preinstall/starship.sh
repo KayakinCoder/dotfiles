2b78383a867a7094925d95f3782ace9f2a60dd9c
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2025-10-24 16:11:41 -0700

    Update starship installation script
    Remove redundant initialization of starship in .bashrc.

--- tools/preinstall/starship.sh
+++ tools/preinstall/starship.sh
@@ -1,9 +1,7 @@
 # starship
 ../../helpers/install-font.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
 curl -sS https://starship.rs/install.sh | sh -s -- -y
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
 cp /workspaces/devcontainer-fd/.build/tools/starship.toml ~/.config/starship.toml
-
 
 # needed for starship fonts to work correctly
 echo 'export LC_ALL="en_US.UTF-8"' >> ~/.bashrc
