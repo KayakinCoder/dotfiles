9812ceeff7bb8449a592e5bd9610d0b32582e9ed
John Aitchison <jaitchison@element84.com>
2025-10-29 14:29:00 -0700

    global

--- tools/preinstall/git-config.sh
+++ tools/preinstall/git-config.sh
@@ -1,5 +1,5 @@
 # Rather than a config file, set values via command line so as not to overwrite any existing settings
-# in a codespace
+# in a codespace. Particularly commiter username
 
 git config --global push.autoSetupRemote true
-git config init.defaultBranch main
+git config --global init.defaultBranch main
