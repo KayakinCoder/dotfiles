6bdffa2b733d6f3804a873ad159f8e55e8e9d33d
John Aitchison <jaitchison@element84.com>
2025-10-29 13:15:49 -0700

    git config

---
+++ tools/preinstall/git-config.sh
@@ -0,0 +1,5 @@
+# Rather than a config file, set values via command line so as not to overwrite any existing settings
+# in a codespace
+
+git config --global push.autoSetupRemote true
+git config init.defaultBranch main
