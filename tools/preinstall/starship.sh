78a757d7ec17532b54ba224de5c72dc9d0780939
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2026-05-18 04:05:19 +0000

    claude-setup-devcontainer

--- tools/preinstall/starship.sh
+++ tools/preinstall/starship.sh
@@ -1,7 +1,6 @@
 # starship
 ../../helpers/install-font.sh https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/FiraCode.zip
 curl -sS https://starship.rs/install.sh | sh -s -- -y
-cp /workspaces/devcontainer-fd/.build/tools/starship.toml ~/.config/starship.toml
 
 # needed for starship fonts to work correctly
 echo 'export LC_ALL="en_US.UTF-8"' >> ~/.bashrc
