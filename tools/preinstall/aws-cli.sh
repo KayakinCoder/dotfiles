d5db92f06ace97a48639a210d4e6f8c40a60d08d
John Aitchison <12837598+KayakinCoder@users.noreply.github.com>
2026-05-19 11:16:16 -0700

    Remove redundant cd command in aws-cli.sh
    Remove unnecessary directory change after AWS CLI installation.

--- tools/preinstall/aws-cli.sh
+++ tools/preinstall/aws-cli.sh
@@ -3,4 +3,3 @@ cd /workspaces && mkdir tempinstall && cd tempinstall
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-cd /workspaces/devcontainer
