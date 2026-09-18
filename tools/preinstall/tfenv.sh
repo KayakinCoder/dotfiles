7237890e27c9d03ce4fcd48036c1e69663c23463
John Aitchison <jaitchison@element84.com>
2026-09-18 10:42:40 -0700

    symlinks

--- tools/preinstall/tfenv.sh
+++ tools/preinstall/tfenv.sh
@@ -1,3 +1,6 @@
 git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
-echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.zshrc
+
+# symlink into /usr/local/bin rather than appending to ~/.bashrc: devcontainer postAttach/postCreate scripts
+# run in a non-interactive shell that never reads ~/.bashrc, but /usr/local/bin is on PATH for every shell.
+# this is tfenv's documented install method and its shims resolve symlinks correctly.
+sudo ln -sf ~/.tfenv/bin/* /usr/local/bin/
