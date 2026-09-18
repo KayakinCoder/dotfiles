43839670bd6d270117d504707371295e4f70f24f
John Aitchison <jaitchison@element84.com>
2026-09-18 10:53:15 -0700

    symlink

--- tools/preinstall/pre-commit.sh
+++ tools/preinstall/pre-commit.sh
@@ -1,1 +1,5 @@
 pipx install pre-commit
+
+# pipx installs to ~/.local/bin, which is only on PATH for interactive shells. expose it to the
+# non-interactive devcontainer startup scripts too.
+sudo ln -sf ~/.local/bin/pre-commit /usr/local/bin/pre-commit
